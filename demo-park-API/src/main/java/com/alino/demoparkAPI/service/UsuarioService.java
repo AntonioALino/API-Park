452768784cca5083cd56676aaea0da64b67a66b4
AntonioALino <antonioaplino@gmail.com>
2024-03-14 00:19:13 -0300

    Change password and find all created

--- demo-park-API/src/main/java/com/alino/demoparkAPI/service/UsuarioService.java
+++ demo-park-API/src/main/java/com/alino/demoparkAPI/service/UsuarioService.java
@@ -1,8 +1,11 @@
 package com.alino.demoparkAPI.service;
 
 import java.util.UUID;
+import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.alino.demoparkAPI.entity.Usuario;
@@ -13,17 +16,29 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
-    
+
     private final UsuarioRepository usuarioRepository;
-    
+
     @org.springframework.transaction.annotation.Transactional
-    public Usuario salvar(Usuario usuario){
+    public Usuario salvar(Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
 
+    @Transactional(readOnly = true)
+    public List<Usuario> enctTD(){
+        return usuarioRepository.findAll();
+    }
+
     @org.springframework.transaction.annotation.Transactional(readOnly = true)
-    public Usuario enctId(UUID uuid){
+    public Usuario enctId(UUID uuid) {
         return usuarioRepository.findById(uuid).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
+
+    @Transactional
+    public Usuario altSenha(UUID uuid, String senha) {
+        Usuario user = enctId(uuid);
+        user.setSenha(senha);
+        return user;
     }
 
 }
--- demo-park-API/src/main/java/com/alino/demoparkAPI/web/UsuarioController.java
+++ demo-park-API/src/main/java/com/alino/demoparkAPI/web/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.alino.demoparkAPI.web;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,7 @@ import com.alino.demoparkAPI.service.UsuarioService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -32,10 +34,22 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
+    @GetMapping
+    public ResponseEntity <List<Usuario>> getAll(){
+       List<Usuario>  user = usuarioService.enctTD();
+        return ResponseEntity.ok(user);
+    }
+
     @GetMapping("/{UUID}")
     public ResponseEntity<Usuario> getUsuarioByID(@PathVariable UUID UUID){
         Usuario user = usuarioService.enctId(UUID);
         return ResponseEntity.status(HttpStatus.FOUND).body(user);
     }
 
+
+    @PatchMapping("/{UUID}")
+    public ResponseEntity<Usuario> changePassword(@PathVariable UUID UUID, @RequestBody Usuario usuario){
+        Usuario user = usuarioService.altSenha(UUID, usuario.getSenha());
+        return ResponseEntity.ok(usuario);
+    }
 }
